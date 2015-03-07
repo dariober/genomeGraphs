a3ec926cd2dd81e124859730fa2206630c5efe98
dariober <dario.beraldi@gmail.com>
2015-03-07 18:13:23 +0000

    Removed files not belonging to genomeGraphs

--- eclipse/Sandbox/src/AutomatonRegex.java
+++
@@ -1,9 +0,0 @@
-
-public class AutomatonRegex {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- roslin/20091020_bowtie_hg95ctrls_ss9.sh
+++
@@ -1,6 +0,0 @@
-/exports/work/vet_roslin_nextgen/dario/bowtie/bowtie-0.11.3/bowtie \
-    Sscrofa9.53.bowtie-index                                       \
-    -f /exports/work/vet_roslin_nextgen/dario/fastq/hg95ctrls.fa   \
-    -k 1                                                           \
-    --best                                                         \
-    /exports/work/vet_roslin_nextgen/dario/bowtie/output/bwt_hg95ctrls_ss9.map
